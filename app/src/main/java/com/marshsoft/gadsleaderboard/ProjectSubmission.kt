d383a3d43529f823c611e225641cd7453e0fdddb
Marshall Musyimi <marshallmusyimi@gmail.com>
2020-09-11 18:14:32 +0300

    Updated layout of ProjectSubmission.kt

--- app/src/main/java/com/marshsoft/gadsleaderboard/ProjectSubmission.kt
+++ app/src/main/java/com/marshsoft/gadsleaderboard/ProjectSubmission.kt
@@ -1,5 +1,10 @@
 package com.marshsoft.gadsleaderboard
 
+import android.app.ActionBar
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +13,11 @@ class ProjectSubmission : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project_submission)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setCustomView(R.layout.custom_actionbar)
+        supportActionBar?.setDisplayShowCustomEnabled(true)
 
     }
 
