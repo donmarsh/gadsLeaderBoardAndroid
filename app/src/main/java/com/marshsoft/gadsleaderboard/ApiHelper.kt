2ea36641464df968680b1d6f4deb8cadd71bcda2
Marshall Musyimi <marshallmusyimi@gmail.com>
2020-09-12 11:51:52 +0300

    Updated dialogs and post to api function

--- app/src/main/java/com/marshsoft/gadsleaderboard/ApiHelper.kt
+++ app/src/main/java/com/marshsoft/gadsleaderboard/ApiHelper.kt
@@ -1,12 +1,23 @@
 package com.marshsoft.gadsleaderboard
 
+import android.R
+import android.content.Context
+import android.net.Uri
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONArray
+
 
 class ApiHelper {
     private val baseApiUrl = "https://gadsapi.herokuapp.com"
     private val skillUrl = "/api/skilliq"
     private val hoursUrl = "/api/hours"
-    fun buildUrl(skillSearch:Boolean = true):String{
+    fun buildUrl(skillSearch: Boolean = true):String{
         return if(skillSearch){
             baseApiUrl+skillUrl
         } else{
@@ -14,21 +25,64 @@ class ApiHelper {
         }
     }
 
-    fun getLeadersFromJson(json:String):ArrayList<Leader>{
+    fun getLeadersFromJson(json: String):ArrayList<Leader>{
         val jsonLeaders = JSONArray(json)
 
         val leaders = ArrayList<Leader>()
         for(x in 0 until jsonLeaders.length()){
             val leaderJson = jsonLeaders.getJSONObject(x)
 
-            val leader = Leader(leaderJson.getString("name"),
-                if(leaderJson.isNull("hours")) 0 else leaderJson.getInt("hours"),
-                if(leaderJson.isNull("score")) 0 else leaderJson.getInt("score"),
+            val leader = Leader(
+                leaderJson.getString("name"),
+                if (leaderJson.isNull("hours")) 0 else leaderJson.getInt("hours"),
+                if (leaderJson.isNull("score")) 0 else leaderJson.getInt("score"),
                 leaderJson.getString("country"),
-                leaderJson.getString("badgeUrl"))
+                leaderJson.getString("badgeUrl")
+            )
             leaders.add(leader)
 
         }
         return leaders
+    }
+    fun submitProject(
+        context: Context,
+        email: String,
+        firstName: String,
+        lastName: String,
+        projectLink: String
+    ):Boolean
+    {
+        var responseBoolean = false;
+        val postUrl = "https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse"
+        val params = HashMap<String, String>()
+        params["entry.1824927963"] = email
+        params["entry.1877115667"] = firstName
+        params["entry.2006916086"] = lastName
+        params["entry.284483984"] = projectLink
+        val headers = HashMap<String, String>()
+        headers.put("Content-Type", "application/x-www-form-urlencoded")
+        val queue = Volley.newRequestQueue(context)
+
+        val request: StringRequest = object : StringRequest( Method.POST, postUrl,
+            Response.Listener { response ->
+                responseBoolean = true
+
+            },
+            Response.ErrorListener { error ->
+                responseBoolean = false
+            }) {
+            override fun getParams(): Map<String, String> {
+
+                return params
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                return headers
+            }
+        }
+
+
+        queue.add(request)
+        return responseBoolean
     }
 }
--- app/src/main/java/com/marshsoft/gadsleaderboard/ProjectSubmission.kt
+++ app/src/main/java/com/marshsoft/gadsleaderboard/ProjectSubmission.kt
@@ -1,23 +1,90 @@
 package com.marshsoft.gadsleaderboard
 
 import android.app.ActionBar
+import android.app.AlertDialog
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_project_submission.*
 
 class ProjectSubmission : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project_submission)
+        btnSubmit.setOnClickListener { showConfirmationDialog() }
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setCustomView(R.layout.custom_actionbar)
         supportActionBar?.setDisplayShowCustomEnabled(true)
+
+    }
+
+    private fun showConfirmationDialog() {
+        val viewGroup = findViewById<ViewGroup>(android.R.id.content)
+        val confirmationDialogView = LayoutInflater.from(this).inflate(R.layout.custom_confirmation_dialog,viewGroup,false)
+        val alertBuilder = AlertDialog.Builder(this)
+        alertBuilder.setView(confirmationDialogView)
+        val alertDialog = alertBuilder.create()
+        alertDialog.show()
+        val button = alertDialog.findViewById<Button>(R.id.btnConfirm)
+        button.setOnClickListener {
+            //send code to api
+            alertDialog.dismiss()
+            val postUrl = "https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse"
+
+
+            val queue = Volley.newRequestQueue(this)
+
+            val request: StringRequest = object : StringRequest( Method.POST, postUrl,
+                Response.Listener { response ->
+                    val successDialog = LayoutInflater.from(this).inflate(R.layout.custom_success_dialog,viewGroup,false)
+                    val sucessAlertBuilder = AlertDialog.Builder(this)
+                    sucessAlertBuilder.setView(successDialog)
+                    val successAlertDialog = sucessAlertBuilder.create()
+                    successAlertDialog.show()
+
+                },
+                Response.ErrorListener { error ->
+                    val failureDialog = LayoutInflater.from(this).inflate(R.layout.error_submission,viewGroup,false)
+                    val failureAlertBuilder = AlertDialog.Builder(this)
+                    failureAlertBuilder.setView(failureDialog)
+                    val failureAlertDialog = failureAlertBuilder.create()
+                    failureAlertDialog.show()
+                }) {
+                override fun getParams(): Map<String, String> {
+
+                    val params = HashMap<String, String>()
+                    params["entry.1824927963"] = etEmail.text as String
+                    params["entry.1877115667"] = etFirstName.text as String
+                    params["entry.2006916086"] = etLastName.text as String
+                    params["entry.284483984"] = etGithubLink.text as String
+                    return params
+                }
+
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["Content-Type"] = "application/x-www-form-urlencoded"
+                    return headers
+                }
+            }
+
+
+            queue.add(request)
+        }
+        val closeImage = alertDialog.findViewById<ImageView>(R.id.imgClose)
+        closeImage.setOnClickListener { alertDialog.dismiss() }
 
     }
 
